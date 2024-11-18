b9c183704a4191f1741554b8fca716b57b646ecf
Ivan <ilyaselmadni1@gmail.com>
2024-11-18 17:42:44 +0100

    server linked to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Friendzy API is working!');
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'rootroot',
+  database: 'friendzy',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+  } else {
+    console.log('Connected to the MySQL database!');
+  }
+});
+
+app.get('/api/questions', (req, res) => {
+  db.query('SELECT * FROM questions', (err, results) => {
+    if (err) {
+      console.error('Error fetching questions:', err);
+      return res.status(500).send('Server error');
+    }
+    res.json(results);
+  });
 });
 
 app.listen(PORT, () => {
